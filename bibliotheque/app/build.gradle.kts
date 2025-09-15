734d59023be94642e59f11f59bdd0743a5d251d7
MaxenMSF <alexandremione@gmail.com>
2025-09-15 17:19:33 +0200

    :sparkles: moitié du tp2 fait, spring initialisé

--- bibliotheque/app/build.gradle.kts
+++ bibliotheque/app/build.gradle.kts
@@ -7,7 +7,9 @@
 
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
-    application
+    java
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.5.5"
     jacoco
 }
 
@@ -24,6 +26,7 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -33,16 +36,7 @@ java {
     }
 }
 
-application {
-    // Define the main class for the application.
-    mainClass = "org.example.App"
-}
-
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
-}
-
-tasks.getByName<JavaExec>("run") {
-    standardInput = System.`in`
 }
